dcdd397cb8032e48a3c99a5759624f1b0182577d
Janaka Kavindu <janakakavindu15@gmail.com>
2025-08-15 09:16:55 +0530

    customer update dao

--- src/main/java/com/example/pahanaeduwebapplication/dao/CustomerDAO.java
+++ src/main/java/com/example/pahanaeduwebapplication/dao/CustomerDAO.java
@@ -21,4 +21,19 @@ public class CustomerDAO {
             return false;
         }
     }
+    public boolean updateCustomer(Customer customer) {
+        String sql = "UPDATE customers SET name=?, address=?, telephone=?, email=? WHERE account_no=?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, customer.getName());
+            stmt.setString(2, customer.getAddress());
+            stmt.setString(3, customer.getTelephone());
+            stmt.setString(4, customer.getEmail());
+            stmt.setInt(5, customer.getAccountNo());
+            return stmt.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
